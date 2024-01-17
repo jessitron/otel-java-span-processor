246d036fb6e5869b0ead257a46ffd1967cf09b04
Jessica Kerr <jessitron@honeycomb.io>
2024-01-17 16:42:34 -0600

    Make the spanprocessor add thread counts
    and remove a lot of dependencies. This makes the jar much nicer

--- src/main/java/com/example/javaagent/DemoSpanProcessor.java
+++ src/main/java/com/example/javaagent/DemoSpanProcessor.java
@@ -10,7 +10,6 @@ import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
-import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * See <a
@@ -23,12 +22,21 @@ public class DemoSpanProcessor implements SpanProcessor {
 
   @Override
   public void onStart(Context parentContext, ReadWriteSpan span) {
-    /*
-    The sole purpose of this attribute is to introduce runtime dependency on some external library.
-    We need this to demonstrate how extension can use them.
-     */
-    span.setAttribute("random", RandomStringUtils.random(10));
-    span.setAttribute("custom", "demo");
+    addGlobalThreadCountToSpan(span);
+  }
+
+  private void addGlobalThreadCountToSpan(ReadWriteSpan span) {
+    // Get the current thread's thread group
+    ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
+    span.setAttribute("thread.group.name", rootGroup.getName());
+    span.setAttribute("thread.group.active.count", rootGroup.activeCount());
+    ThreadGroup parentGroup;
+    while ((parentGroup = rootGroup.getParent()) != null) {
+        rootGroup = parentGroup;
+    }
+    // Active count of all threads 
+    span.setAttribute("thread.root.active.count", rootGroup.activeCount());
+    // System.out.println("Number of active threads: " + activeThreads);
   }
 
   @Override
